af13d5caad6c206153497bc4bc21787cd364bebc
Ricardo Cuellar <richi_gcuellar@outlook.com>
2020-04-20 00:35:31 -0500

    se modifican archivos

--- programa.js
+++ programa.js
@@ -1,6 +1,3 @@
-const l=[];
-const l1=[];
-
 /* Textos */
 const a=document.getElementById("nom");
 const n1=document.getElementById("num1");
@@ -8,7 +5,6 @@ const n1=document.getElementById("num1");
 /* Párrafos */
 const r=document.getElementById("n1");
 const nm1=document.getElementById("nm1");
-const nm2=document.getElementById("nm2");
 
 /* Botones */
 const bot=document.getElementById("b1");
@@ -17,24 +13,22 @@ if (bot)
     bot.addEventListener("click", escribirNombre);
 }
 
-const bV=document.getElementById("b2");
-if (bV)
+const b2=document.getElementById("b2");
+if (b2)
 {
-    bV.addEventListener("click", d_B)
+    b2.addEventListener("click", conversorDecimal)
 }
-
 
 /* h2 */
 const h2=document.getElementById("h2");
 
-function d_B()
+function conversorDecimal()
 {   
-    const numero=parseInt(n1.value);
-    nm1.innerHTML="Su número ingresado: " + numero.toString() + "<br />Su número en binario: " + numero.toString(2)
-    + "<br />Su número en Octal: " + numero.toString(8) + "<br />Su número en Hexadecimal: " + numero.toString(16);
+    const numero=n1.value;
+    nm1.innerHTML="<br />Su número en binario: " + numero.toString(2) + "<br />Su número en Octal: " 
+    + numero.toString(8) + "<br />Su número en Hexadecimal: " + numero.toString(16);
 }
-
-
+                                       
 function escribirNombre()
 {
     r.innerHTML="¡Bienvenido " + a.value +"!";
